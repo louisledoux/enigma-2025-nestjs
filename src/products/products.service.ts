eb0b6bc4053fa1857e600043d1fc44ae53560f67
Louis Ledoux <19322697+louisledoux@users.noreply.github.com>
2025-12-19 10:33:10 +0100

    feat(products): get products by ID

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,48 @@
 import { Injectable } from '@nestjs/common';
 
+interface Product {
+  id: number;
+  documentId: string;
+  title: string;
+  description: string;
+  price: number;
+  stock: number;
+  image: {
+    url: string;
+  };
+}
+
+interface ProductResponse {
+  data: Product[];
+}
+
 @Injectable()
 export class ProductsService {
+  private baseUrl = 'https://strapi-production-2ff5.up.railway.app';
+
   getProducts() {
-    return fetch(
-      'https://strapi-production-2ff5.up.railway.app/api/poke-products?populate=*',
-    ).then((res) => res.json());
+    return fetch(`${this.baseUrl}/api/creatine-products?populate=*`).then(
+      (res) =>
+        res.json().then((data: ProductResponse) => ({
+          ...data,
+          data: data.data.map((item: Product) => ({
+            ...item,
+            image: `${this.baseUrl}${item.image.url}`,
+          })),
+        })),
+    );
+  }
+
+  // 2 endpoints avec les méthodes dans le service associé:
+  // - /products
+  // - /products/:id
+  getProduct(id: string) {
+    return fetch(`${this.baseUrl}/api/creatine-products/${id}?populate=*`).then(
+      (res) =>
+        res.json().then(({ data }: { data: Product }) => ({
+          ...data,
+          image: `${this.baseUrl}${data.image.url}`,
+        })),
+    );
   }
 }
